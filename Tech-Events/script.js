010cb58977b7cd86e57eac3b75437f8756683bb7
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2024-02-05 02:04:19 +0530

    modified

--- Tech-Events/script.js
+++ Tech-Events/script.js
@@ -66,14 +66,28 @@ const scroll = new LocomotiveScroll({
 
 // mouse Follower
 
-function mouseFollower() {
-  window.addEventListener("mousemove", (dets) => {
-    // console.log(dets.clientX)
-    document.querySelector(
-      ".cursor"
-    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-  });
-}
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+const Page3 = document.querySelector("#page3");
+
+window.addEventListener("mousemove", (e) => {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  cursorOutline.style.left = `${posX}px`;
+  cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+});
 
 function text() {
   var tl = gsap.timeline();
